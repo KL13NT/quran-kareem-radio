eee66c629c4dbaeda6ca346e95c6c686b8b5bf0c
Nabil Tharwat <nabil.tharwat@outlook.com>
2022-05-11 19:06:27 +0200

    fix: bot deletes old session but doesn't create new one on move
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- index.js
+++ index.js
@@ -61,14 +61,24 @@ client.once("ready", async () => {
 
   client.on("voiceStateUpdate", async (oldState, newState) => {
     if (newState.member.id === CLIENT_ID && !newState.channel) {
+      console.log(
+        `bot disconnected from ${oldState.guild.name} ${oldState.channel.name}`
+      );
+
       await redis.del(`CONNECTION:${oldState.guild.id}:${oldState.channel.id}`);
     } else if (
       newState.member.id === CLIENT_ID &&
       newState.channel &&
       oldState.channel
     ) {
-      await redis.del(`CONNECTION:${oldState.guild.id}:${oldState.channel.id}`);
-      await redis.set(`CONNECTION:${newState.guild.id}:${newState.channel.id}`);
+      console.log(
+        `bot has been moved from ${oldState.guild.name} ${oldState.channel.name} to ${newState.channel.name}`
+      );
+
+      await Promise.all([
+        redis.del(`CONNECTION:${oldState.guild.id}:${oldState.channel.id}`),
+        redis.set(`CONNECTION:${newState.guild.id}:${newState.channel.id}`, 0),
+      ]);
     }
   });
 
